0ead12ab7f1af139bf5a61cefe0386afcf4167dc
Rondon Guilherme <rondonguivas333@gmail.com>
2025-03-18 09:51:04 -0300

    Botões na aba react

--- Testes Iniciais/aba-react/src/App.jsx
+++ Testes Iniciais/aba-react/src/App.jsx
@@ -1,15 +1,42 @@
-//import { useState } from 'react'
+import { useState } from "react";
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
-import './App.css'
+//import "./App.css";
 
 import { Cabecalho } from "./components/Cabecalho";
 
 function App() {
+  const [contador, setContador] = useState(0);
+  const [incremento, setIncremento] = useState(1);
+
+  function handleBtIncrementarClick() {
+    setContador(contador + 1);
+  }
+
+  function handleBtIncrementarValorClick() {
+    setContador(contador + incremento);
+  }
+
+  function handleBtLimparClick() {
+    setContador(0);
+  }
+
+  function handleIncrementoChange(evt) {
+    setIncremento(parseInt(evt.target.value));
+  }
+
   return (
     <>
       <Cabecalho />
-      <p>Teste de Paragrafo</p>
+      <p>Quantidades de cliques: {contador}</p>
+      <button onClick={handleBtIncrementarClick}>Incrementar</button>
+      <button onClick={handleBtLimparClick}>Limpar</button>
+      <input
+        type="number"
+        value={incremento}
+        onChange={handleIncrementoChange}
+      />
+      <button onClick={handleBtIncrementarValorClick}>Incrementar Valor</button>
     </>
   );
 }
