e8ec45b51d5d755265e2809e74c1e10ddc721b4c
Rondon Guilherme <rondonguivas333@gmail.com>
2025-03-21 14:51:22 -0300

    Fetch

---
+++ Testes Iniciais/aba-starwars/src/components/Cabecalho.jsx
@@ -0,0 +1,11 @@
+export function Cabecalho() {
+    console.log("Executando a função Cabecalho!");
+  
+    return (
+      <header>
+        <h1>Programação Web e Mobile</h1>
+        <h2>Curso de Ciência da Computação - UNICAP</h2>
+        <hr />
+      </header>
+    );
+  }
